58cb94c8db27334b2e23d8b47deb1201b28daa5c
Karen <karen@bacawik.com>
2026-01-05 13:57:04 +0100

    update page based on new refList structure

--- contributions.py
+++ contributions.py
@@ -1,19 +1,28 @@
 import streamlit as st
+from content import RefList
 
-# --- GENERAL SETTINGS ---
+
 st.set_page_config(page_title="Contributions", 
                    page_icon="🏆",
                    layout="centered", 
                    initial_sidebar_state="expanded") 
 
 
-st.header("🏆 Contributions")
+st.header("Conferences and Publications")
 
-st.subheader("Conferences")
-
+st.subheader(RefList.neo4j_video.md)
 st.video(data = "https://www.youtube.com/watch?v=RH3-Lv9p9MU",
-         start_time = "11m15s",
+         start_time = "9m33s",
          loop=False,
          autoplay=True)
 
-st.subheader("Publications")
+st.subheader(RefList.plp.md)
+st.markdown(RefList.plp.citation, unsafe_allow_html=True)
+
+
+st.subheader(RefList.genie_pub.md)
+st.markdown(RefList.genie_pub.citation, unsafe_allow_html=True)
+
+
+st.subheader(RefList.masterThesis.md)
+st.markdown(RefList.masterThesis.citation, unsafe_allow_html=True)
